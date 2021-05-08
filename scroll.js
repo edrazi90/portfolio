862b6f4249b09d03cfa5b23e98c01dc751993db3
edrazi90 <edrazi90@gmail.com>
2021-05-09 03:42:22 +0800

    scroll

---
+++ scroll.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+$(window).scroll(function(){
+
+    if(this.scroll>20){
+
+        $('.header').addclass("sticky");
+
+    }else{
+        $('.header').addclass("sticky");
+    }
+})
+
+
+});
